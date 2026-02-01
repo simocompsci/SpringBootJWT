9e57b8a1f15a7b7cbf027f02fe8cb52d2be83fa7
simocompsci <hamids9alo7777@gmail.com>
2026-02-01 18:47:51 +0100

    working through the jwt service

--- src/main/java/com/Auth/JWT/config/ApplicationConfig.java
+++ src/main/java/com/Auth/JWT/config/ApplicationConfig.java
@@ -1,8 +1,22 @@
 package com.Auth.JWT.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.Auth.JWT.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@RequiredArgsConstructor
 public class ApplicationConfig {
-    
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> userRepository.findByUsername(username)
+                                        .orElseThrow(()-> new UsernameNotFoundException("User not found"));
+    }
 }
