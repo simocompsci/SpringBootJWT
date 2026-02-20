4c7dc732bb45672ca95743fb0c184e933ede9cce
simocompsci <hamids9alo7777@gmail.com>
2026-02-20 14:50:02 +0000

    adding remaining auth files

---
+++ src/main/java/com/Auth/JWT/Respository/UserInfoRepository.java
@@ -0,0 +1,13 @@
+package com.Auth.JWT.Respository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Auth.JWT.Entity.UserInfo;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByEmail(String email); // Use 'email' if that is the correct field for login
+}
